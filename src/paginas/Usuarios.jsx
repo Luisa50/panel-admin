c0ce0d30822dc22a4b33f32ea10d771ec2d34760
unknown <camilovillalobos252@gmail.com>
2025-11-30 00:24:06 -0500

    validaciones, paginacion, y datos

--- src/paginas/Usuarios.jsx
+++ src/paginas/Usuarios.jsx
@@ -61,30 +61,25 @@ const handleEliminar = async (id) => {
   });
 
   alert("Eliminado");
-  // Recargar tabla
 };
 
 export default function Usuarios() {
   DataTable.use(DT);
-  // const [query, setQuery] = useState("");
-  // const [pagina, setPagina] = useState(1);
-  // const porPagina = 10;
-
-
-  //const [usuarios, setUsuarios] = useState(generarUsuariosMock());
   const [usuarios, setUsuarios] = useState([]);
+  const [informacion, setInformacion] = useState([])
+  const [cantidadReg, setCantidadReg] = useState([])
 
+  const loadData = async (pag = 1, lengthPag = 5) => {
+        await fetch(`http://healthymind10.runasp.net/api/Aprendiz/listar?Pagina=${pag}&TamanoPagina=${lengthPag}`)
+        .then(res => res.json())
+        .then(json => {setUsuarios(json.resultado)
+          setInformacion(json)
+          console.log(json.resultado);
+          
+        })
+      }
   useEffect(() => {
-    const loadData = async (pag = 1, lengthPag = 5) => {
-      await fetch(`http://healthymind10.runasp.net/api/Aprendiz/listar?Pagina=${pag}&TamanoPagina=${lengthPag}`)
-      .then(res => res.json())
-      .then(json => {setUsuarios(json.resultado)
-        console.log(json.resultado);
-        
-      })
-    }
-
-    loadData(1, 3)
+    loadData()
   }, [])
 
   const columnas = [
@@ -130,7 +125,7 @@ export default function Usuarios() {
       orderable: false,
       searchable: false,
 
-      createdCell: (td, id, rowData) => {
+      createdCell: (td, id) => {
         const root = ReactDOM.createRoot(td);
         root.render(
           <AccionesAprendiz 
@@ -142,47 +137,23 @@ export default function Usuarios() {
         );
       }
   }
-  ]
-  // const filtrados = useMemo(() => {
-  //   const q = query.toLowerCase().trim();
-  //   if (!q) return usuarios;
+]
 
-  //   return usuarios.filter(
-  //     (u) =>
-  //       u.nombre.toLowerCase().includes(q) ||
-  //       String(u.documento).includes(q) ||
-  //       u.correo.toLowerCase().includes(q)
-  //   );
-  // }, [query, usuarios]);
-
-  // const totalPaginas = Math.ceil(filtrados.length / porPagina);
-  // const mostrar = filtrados.slice((pagina - 1) * porPagina, pagina * porPagina);
-
-
-  // const toggleEstado = (id) => {
-  //   setUsuarios((prev) =>
-  //     prev.map((u) => {
-  //       if (u.id === id) {
-  //         const pregunta =
-  //           u.activo === "Activo"
-  //             ? "¿Deseas inactivar este usuario?"
-  //             : "¿Deseas activar este usuario?";
-
-  //         if (window.confirm(pregunta)) {
-  //           return {
-  //             ...u,
-  //             activo: u.activo === "Activo" ? "Inactivo" : "Activo",
-  //           };
-  //         }
-  //       }
-  //       return u;
-  //     })
-  //   );
-  // };
 
   return (
     <div className="container m-0">
-      <h2>Listado de usuarios</h2>
+      <div className="encabezado">
+        <h2>Listado de usuarios</h2>
+        <select class="seleccionCantidad" onChange={(e) => {
+          const nuevaCantidad = parseInt(e.target.value);
+          setCantidadReg(nuevaCantidad);
+          loadData(informacion.paginaActual, nuevaCantidad)
+        }}>
+          <option value="5" defaultChecked>5</option>
+          <option value="10">10</option>
+          <option value="15">15</option>
+        </select>
+      </div>
 
       <DataTable 
         columns={columnas} 
@@ -200,94 +171,30 @@ export default function Usuarios() {
         >
         
       </DataTable>
-      {/* <div className="d-flex justify-content-end mb-2">
-        <input
-          className="form-control"
-          style={{ width: "220px" }}
-          placeholder="Buscar…"
-          value={query}
-          onChange={(e) => {
-            setQuery(e.target.value);
-            setPagina(1);
-          }}
-        />
+      <div className="btn-group mt-3">
+
+        <button 
+          className="btn btn-outline-primary"
+          disabled={!informacion.paginaAnterior}
+          onClick={() => loadData(informacion.paginaAnterior, cantidadReg)}
+        >
+          Anterior
+        </button>
+
+        <button className="btn btn-primary">
+          {informacion.paginaActual}
+        </button>
+
+        <button 
+          className="btn btn-outline-primary"
+          disabled={!informacion.paginaSiguiente}
+          onClick={() => loadData(informacion.paginaSiguiente, cantidadReg)}
+        >
+          Siguiente
+        </button>
+
       </div>
 
-      <div className="table-responsive">
-        <table className="table table-striped table-bordered text-center">
-          <thead className="table-dark">
-            <tr>
-              <th>#</th>
-              <th>Documento</th>
-              <th>Nombre completo</th>
-              <th>Correo institucional</th>
-              <th>Municipio</th>
-              <th>Estado</th>
-              <th>Activo</th>
-            </tr>
-          </thead>
-
-          <tbody>
-            {mostrar.map((u, i) => (
-              <tr key={u.id}>
-                <td>{(pagina - 1) * porPagina + i + 1}</td>
-                <td>{u.documento}</td>
-                <td>{u.nombre}</td>
-                <td>{u.correo}</td>
-                <td>{u.municipio}</td>
-                <td>{u.estado}</td>
-
-
-                <td
-                  style={{ cursor: "pointer" }}
-                  onClick={() => toggleEstado(u.id)}
-                >
-                  {u.activo === "Activo" ? (
-                    <span className="badge bg-success">Activo</span>
-                  ) : (
-                    <span className="badge bg-danger">Inactivo</span>
-                  )}
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-
-      <nav>
-        <ul className="pagination justify-content-center">
-          <li className="page-item">
-            <button
-              className="page-link"
-              disabled={pagina === 1}
-              onClick={() => setPagina(pagina - 1)}
-            >
-              «
-            </button>
-          </li>
-
-          {[...Array(totalPaginas)].map((_, i) => (
-            <li
-              key={i}
-              className={`page-item ${pagina === i + 1 ? "active" : ""}`}
-            >
-              <button className="page-link" onClick={() => setPagina(i + 1)}>
-                {i + 1}
-              </button>
-            </li>
-          ))}
-
-          <li className="page-item">
-            <button
-              className="page-link"
-              disabled={pagina === totalPaginas}
-              onClick={() => setPagina(pagina + 1)}
-            >
-              »
-            </button>
-          </li>
-        </ul>
-      </nav> */}
     </div>
     
   );
